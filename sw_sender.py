d655d77b042623290bfc1c7792a85f90768bbbdf
wujie <vjiewu@gmail.com>
2019-04-12 09:36:11 +0800

    Commit C/S v2

--- sw_sender.py
+++ sw_sender.py
@@ -5,15 +5,28 @@ FilterError = 10
 FilterLost = 10
 
 
+def filter_frame():
+    return
+
+
 def main():
     ip_port = ('127.0.0.1', UDPPort)
     next_frame_to_send = 0  # seq number of next frame
-    sk = socket.socket()
-    sk.connect(ip_port)
-    sk.sendall(bytes('data_frame', 'utf8'))
-    server_reply = sk.recv(1024)
-    print(str(server_reply, 'utf8'))
-    sk.close()
+    sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    loop = 20
+
+    while loop:
+
+        print('next_frame_to_send: %d' % next_frame_to_send)
+        sk.sendto(bytes('data_frame', 'utf8'), ip_port)
+        try:
+            sk.settimeout(1)  # timeout = 1s
+            data, addr = sk.recvfrom(1024)
+            print(str(data, 'utf8'))
+        except OSError:
+            print('Receive ACK timeout Err!')
+
+        loop -= 1
 
 
 if __name__ == '__main__':
