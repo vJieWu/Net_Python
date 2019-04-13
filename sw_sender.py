08d36d77ad5768db7a1b435e64a1c426973f920d
wujie <vjiewu@gmail.com>
2019-04-13 14:56:59 +0800

    Commit sender v1

--- sw_sender.py
+++ sw_sender.py
@@ -1,32 +1,71 @@
+import random
 import socket
 
+from PyCRC import CRCCCITT
+
+# CRCCCIT.CRCCCITT().calculate(input)
 UDPPort = 8888
 FilterError = 10
 FilterLost = 10
+Frame_head = '01111110'
+Frame_tail = '01111110'
+InfoString = '11111010101010101010101010101111'
+
+ip_port = ('127.0.0.1', UDPPort)
+sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-def filter_frame():
-    return
+def get_frame(frame_to_send):
+    crc = bin(CRCCCITT.CRCCCITT().calculate(int(InfoString, 2)))[1:]
+    frame = Frame_head + frame_to_send + InfoString + crc + Frame_tail
+
+    return frame
+
+
+def filter_frame(frame):
+    if random.randint(1, FilterLost) == 1:
+        print('Frame Lost')
+    else:
+        if random.randint(1, FilterError) == 1:
+            print('Frame Err')
+
+            if frame[9] == '0':
+                frame = frame[:9] + '1' + frame[10:]
+            else:
+                frame = frame[:9] + '0' + frame[10:]
+
+        else:
+            print('Frame Sent Successfully')
+
+        sk.sendto(bytes(str(frame), 'utf8'), ip_port)
 
 
 def main():
-    ip_port = ('127.0.0.1', UDPPort)
-    next_frame_to_send = 0  # seq number of next frame
-    sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     loop = 20
+    next_frame_to_send = 0  # seq number of next frame
 
     while loop:
+        print('current_send_frame: %d' % next_frame_to_send)
 
-        print('next_frame_to_send: %d' % next_frame_to_send)
-        sk.sendto(bytes('data_frame', 'utf8'), ip_port)
         try:
-            sk.settimeout(1)  # timeout = 1s
-            data, addr = sk.recvfrom(1024)
-            print(str(data, 'utf8'))
+
+            filter_frame(get_frame(str(next_frame_to_send)))
+            sk.settimeout(5)
+            print('Expected ACK: %d' % next_frame_to_send)  # timeout = 1s
+            info, addr = sk.recvfrom(1024)
+
+            if str(info, 'utf8')[8] == next_frame_to_send:  ################
+
+                print('Receive ACK: ' + str(info, 'utf8')[8])  # 接受正确的ack，输出相关信息
+
+                next_frame_to_send = 1 - next_frame_to_send
+                print('next_frame_to_send: %d' % next_frame_to_send)
+                loop -= 1
+            else:
+                print('Receive unexpected ACK')
+
         except OSError:
             print('Receive ACK timeout Err!')
-
-        loop -= 1
 
 
 if __name__ == '__main__':
